be4500517d4c1dd4aeae387f23a667e601986fc0
Fedor <f.e.pivovarov@gmail.com>
2025-10-29 22:35:58 +0700

    add initialized spring project

---
+++ backend/src/main/java/ru/pivovardi/teodoro/telebuff/TelebuffApplication.java
@@ -0,0 +1,13 @@
+package ru.pivovardi.teodoro.telebuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelebuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelebuffApplication.class, args);
+	}
+
+}
